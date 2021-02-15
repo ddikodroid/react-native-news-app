65cfc9029b7c2944196f996bd4f89c32caffd855
ddikodroid <ddikodroid@gmail.com>
2021-02-15 16:26:06 +0700

    add color palette

---
+++ src/styles/Color.js
@@ -0,0 +1,11 @@
+export const COLOR = {
+  primary: '#03a9f4',
+  secondary: '#78909c',
+  black: '#2b2b2b',
+  white: '#fafafa',
+  gray: '#f5f5f5',
+  teal: '#26a69a',
+  yellow: '#ffeb3b',
+  transparent: '#00000000',
+  red: '#d32f2f'
+}
