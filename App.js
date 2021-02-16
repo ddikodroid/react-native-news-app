295eb7db74b1cf6bd49fc547cb02cee8cfa7d22c
ddikodroid <ddikodroid@gmail.com>
2021-02-16 14:33:56 +0700

    wrap app with provider

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 
 import React from 'react'
+import { Provider } from 'react-redux'
+import configureStore from './src/redux/store'
 import MainTabNavigator from './src/navigations/MainTabNavigator'
 
 const App = () => {
   return (
-    <MainTabNavigator />
+    <Provider store={configureStore}>
+      <MainTabNavigator />
+    </Provider>
   )
 }
 export default App
