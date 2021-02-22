ef3d644172f88aebb2af487a3f5ff0435e4630c2
ddikodroid <ddikodroid@gmail.com>
2021-02-22 15:32:52 +0700

    main app code

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import globalStore from './src/redux/store'
 import MainTabNavigator from './src/navigations/MainTabNavigator'
+// import MainTabNavigator2 from './src/navigations/MainTabNavigator2'
 
 const App = () => {
   return (
     <Provider store={globalStore}>
       <MainTabNavigator />
     </Provider>
+    // <MainTabNavigator2 />
   )
 }
 export default App
