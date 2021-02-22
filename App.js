dff9528adb341cd3160f425214174b86f92a8bbc
ddikodroid <ddikodroid@gmail.com>
2021-02-22 15:35:26 +0700

    app.js for tutorial

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 
 import React from 'react'
-import { Provider } from 'react-redux'
-import globalStore from './src/redux/store'
-import MainTabNavigator from './src/navigations/MainTabNavigator'
-// import MainTabNavigator2 from './src/navigations/MainTabNavigator2'
+// import { Provider } from 'react-redux'
+// import globalStore from './src/redux/store'
+// import MainTabNavigator from './src/navigations/MainTabNavigator'
+import MainTabNavigator2 from './src/navigations/MainTabNavigator2'
 
 const App = () => {
   return (
-    <Provider store={globalStore}>
-      <MainTabNavigator />
-    </Provider>
-    // <MainTabNavigator2 />
+    // <Provider store={globalStore}>
+    //   <MainTabNavigator />
+    // </Provider>
+    <MainTabNavigator2 />
   )
 }
 export default App
